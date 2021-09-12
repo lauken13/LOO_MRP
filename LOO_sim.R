6ef98f1a8e2873401efa40df5cc5d2a59c0e3008
swenk <swen.kuh@monash.edu>
2021-09-13 09:56:00 +1000

    Update LOO_sim.R

--- LOO_sim.R
+++ LOO_sim.R
@@ -11,7 +11,6 @@ options(mc.cores = parallel::detectCores())
 
 ## generating diff. levels of predictors/covariates
 ## state of residence (6), sex (2),  ethnicity (6), education (4), age (4)
-
 N = 5000
 J = c(6,2,6,4,4) # levels for each variable
 popn_data <- data.frame(state = sample(1:J[1], N, replace= TRUE), 
@@ -29,35 +28,32 @@ popn_data$bin_outcome <- inv_logit_scaled(round(rnorm(J[1], sd=0.5),2)[popn_data
                                        round(rnorm(J[3], sd=0.4),2)[popn_data$eth] +
                                         round(rnorm(J[4], sd=0.2),2)[popn_data$educ] + 
                                          round(rnorm(J[5], sd=0.3),2)[popn_data$age])
-popn_data$bin_value <- rbinom(n,1,popn_data$bin_outcome)
+popn_data$bin_value <- rbinom(N,1,popn_data$bin_outcome)
 hist(popn_data$bin_outcome)
 hist(popn_data$bin_value)
+
+
+## sample from the 'population', with inclusion prob.
+# generate inclusion prob. for each variable
+popn_data$inclusion <- inv_logit_scaled(round(rnorm(J[1], sd=0.5),2)[popn_data$state] + # apply inv-logit for 'simulated' coefficients
+                                            round(rnorm(J[2], sd=0.35),2)[popn_data$sex] +
+                                            round(rnorm(J[3], sd=0.4),2)[popn_data$eth] +
+                                            round(rnorm(J[4], sd=0.2),2)[popn_data$educ] + 
+                                            round(rnorm(J[5], sd=0.3),2)[popn_data$age])
+hist(popn_data$inclusion)
+
+## checking the inclusion probabilities
+popn_data[which(popn_data$inclusion == popn_data$inclusion[2]),]
 
 ## factorise the relevant variables
 popn_data[,c(1:5,7)] = apply(popn_data[,c(1:5,7)], 2, function(x)as.factor(x))
 str(popn_data)
 
-## sample from the 'population', with inclusion prob.
-# generate inclusion prob. for each variable
-incl_prob = list()
-incl_prob$p_state = rbeta(J[1], 1, 1)
-incl_prob$p_sex = (rbeta(J[2], 1, 1))
-incl_prob$p_eth = (rbeta(J[3], 1, 1))
-incl_prob$p_educ = (rbeta(J[4], 1, 1))
-incl_prob$p_age = (rbeta(J[5], 1, 1))
-
-incl_prob1 = lapply(incl_prob, prop.table) # create probabilities
-
-(incl_prob2 = lapply(incl_prob1, sum)) # check that the prob sums to 1
-
-p_response <- rep(NA, prod(J))
-
-## need to include inclusion probability 
-n = 1000
-ind = sample(1:nrow(popn_data), size=n, replace=T)
-samp_data = popn_data[ind,]
-
-
+## taking a sample data according to the inclusion probability
+samp_size = 1000
+samp_loc = sample(1:nrow(popn_data), size = samp_size, replace=F, prob = popn_data$inclusion)
+samp_data = popn_data[samp_loc,]
+str(samp_data)
 
 ## creating survey design
 svy1 = svydesign(ids=~1, # cluster id, ~1 for no clusters
@@ -82,7 +78,7 @@ samp_data$wts = weights(rake1)
 model1 = brm(bin_value ~ (1|state) + factor(sex) + (1|eth) +
                (1|age) + (1|educ), data = samp_data,
              backend = "cmdstanr",
-             family = bernoulli(link = "logit"), 
+             family = bernoulli(link = "logit"), weight = samp_data$wts,
              control = list(adapt_delta = 0.99)) 
 
 model2 = brm(bin_value ~ factor(sex) + (1|eth) +
@@ -98,7 +94,7 @@ loo2 <- loo(model2)
 
 # Get the elpd
 samp_data$elpd_loo1 <- loo1$pointwise[,1]
-sum(elpd_loo1)
+sum(samp_data$elpd_loo1)
 
 # # If you have weights
 # weighted_sum <- sum(elpd_loo1*samp_data$wts)
@@ -108,7 +104,7 @@ svy_rake = svydesign(ids=~1, # cluster id, ~1 for no clusters
                  weights=~wts, # including raked weights in the survey design
                  data=samp_data)
 
-svytotal(~elpd_loo1, svy_rake) # weighted elpd
+svytotal(~samp_data$elpd_loo1, svy_rake) # weighted elpd
 
 
 

5142e336c27e13de19155ed61f2455c3b1c2422c
swenk <swen.kuh@monash.edu>
2021-09-20 11:21:25 +1000

    rename folders

--- 01-code/LOO_sim.R
+++ 01-code/LOO_sim.R
@@ -2,6 +2,7 @@
 #### to mimic Gelman and Little (1997)
 ### also follows https://cran.r-project.org/web/packages/rstanarm/vignettes/mrp.html#effect-of-the-post-stratification-variable-on-preference-for-cats
 ### 1/9/2021 
+
 library(tidyverse)
 library(brms) # need cmdstanr to use with brms
 library(loo) # calculating loo and elpd
@@ -34,16 +35,16 @@ hist(popn_data$bin_value)
 
 
 ## sample from the 'population', with inclusion prob.
-# generate inclusion prob. for each variable
-popn_data$inclusion <- inv_logit_scaled(round(rnorm(J[1], sd=0.5),2)[popn_data$state] + # apply inv-logit for 'simulated' coefficients
-                                            round(rnorm(J[2], sd=0.35),2)[popn_data$sex] +
-                                            round(rnorm(J[3], sd=0.4),2)[popn_data$eth] +
-                                            round(rnorm(J[4], sd=0.2),2)[popn_data$educ] + 
-                                            round(rnorm(J[5], sd=0.3),2)[popn_data$age])
+# generate inclusion prob. for each individual
+popn_data$inclusion <- inv_logit_scaled(round(rnorm(J[1], sd=0.2),2)[popn_data$state] + # apply inv-logit for 'simulated' coefficients
+                                            round(rnorm(J[2], sd=0.5),2)[popn_data$sex] +
+                                            round(rnorm(J[3], sd=0.2),2)[popn_data$eth] +
+                                            round(rnorm(J[4], sd=0.1),2)[popn_data$educ] + 
+                                            round(rnorm(J[5], sd=0.4),2)[popn_data$age])
 hist(popn_data$inclusion)
 
 ## checking the inclusion probabilities
-popn_data[which(popn_data$inclusion == popn_data$inclusion[2]),]
+popn_data[which(popn_data$inclusion == popn_data$inclusion[5]),]
 
 ## factorise the relevant variables
 popn_data[,c(1:5,7)] = apply(popn_data[,c(1:5,7)], 2, function(x)as.factor(x))
@@ -78,8 +79,8 @@ samp_data$wts = weights(rake1)
 model1 = brm(bin_value ~ (1|state) + factor(sex) + (1|eth) +
                (1|age) + (1|educ), data = samp_data,
              backend = "cmdstanr",
-             family = bernoulli(link = "logit"), weight = samp_data$wts,
-             control = list(adapt_delta = 0.99)) 
+             family = bernoulli(link = "logit"), 
+             control = list(adapt_delta = 0.99))
 
 model2 = brm(bin_value ~ factor(sex) + (1|eth) +
                (1|age) + (1|educ), data = samp_data,
@@ -88,13 +89,20 @@ model2 = brm(bin_value ~ factor(sex) + (1|eth) +
              control = list(adapt_delta = 0.99)) 
 
 
-# Use loo
+# calculating loo
 loo1 <- loo(model1)
 loo2 <- loo(model2)
 
-# Get the elpd
+loo_compare(list(loo1,loo2))
+
+# getting the elpd
 samp_data$elpd_loo1 <- loo1$pointwise[,1]
-sum(samp_data$elpd_loo1)
+samp_data$elpd_loo2 <- loo2$pointwise[,1]
+sum(samp_data$elpd_loo)
+samp_data$elpd_diff = samp_data$elpd_loo2 - samp_data$elpd_loo1
+
+## manual calculation of the std error of diff
+# sqrt(samp_size) * sd(loo2$pointwise[,1] - loo1$pointwise[,1]) 
 
 # # If you have weights
 # weighted_sum <- sum(elpd_loo1*samp_data$wts)
@@ -104,7 +112,13 @@ svy_rake = svydesign(ids=~1, # cluster id, ~1 for no clusters
                  weights=~wts, # including raked weights in the survey design
                  data=samp_data)
 
-svytotal(~samp_data$elpd_loo1, svy_rake) # weighted elpd
+svytotal(~samp_data$elpd_diff, svy_rake) # weighted elpd
 
 
+## checking using svytotal() and loo_compare() for equal weights
+svy_rake1 = svydesign(ids=~1, # cluster id, ~1 for no clusters
+                     weights=~1, # including raked weights in the survey design
+                     data=samp_data)
 
+svytotal(~samp_data$elpd_diff, svy_rake1) # weighted elpd
+loo_compare(loo1, loo2)

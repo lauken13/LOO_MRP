00cdd7571077be69166dffa84a93dcb85c5ea813
swenk <swen.kuh@monash.edu>
2021-12-13 21:39:38 +1100

    new files

---
+++ 02-super popn experiment/01-code/popn2/experiment3b/read_bash_loop.R
@@ -0,0 +1,23 @@
+## loading first iteration of the bash loop results
+setwd("~/GitHub/LOO_MRP/02-super popn experiment/02-data/popn2/experiment3b")
+load("simulated_1.RData")
+
+coef_list_list = list(coef_list)
+elpd_popnest_list = list(elpd_popnest_all)
+samp_data_list = list(samp_data)
+samp_data2_list = list(samp_data2)
+sim_list1 = list(sim_list)
+pt_list = prob_truth
+
+# loading the rest of the results
+for(ite in c(2:64,66:76,78:100)) {
+  load(paste0("simulated_",ite,".RData")) 
+  coef_list_list[[ite]] = coef_list
+  elpd_popnest_list[[ite]] = elpd_popnest_all
+  samp_data_list[[ite]] = samp_data
+  samp_data2_list[[ite]] = samp_data2
+  sim_list1[[ite]] = sim_list
+  pt_list[ite] = prob_truth
+}
+
+

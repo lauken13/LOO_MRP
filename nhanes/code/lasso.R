c9d0a83e5fac28a98fc950aa0f21e7a924554b2e
swenk238 <swen.kuh@monash.edu>
2022-10-31 11:04:22 +1100

    lasso for selecting variables

--- nhanes/code/lasso.R
+++ nhanes/code/lasso.R
@@ -1,37 +1,72 @@
+## Data cleaning
+library(here)
+library(foreign) # read.xport()
+library(tidyverse)
+library(questionr) # recode.na()
+library(GGally) # ggpairs()
+library(forcats) #fct_relevel
 library(glmnet)
 
-nhdata = readRDS(file="nhanes/data/nhdata_full.rds")
+dat4 = readRDS(file=here("nhanes/data/nhdata_full.rds"))
 
-names(nhdata)
-m = model.matrix(~., nhdata[,covind])
+# checking correlation with inclusion
+corIncl = c(2:28)
+dat4 = dat4 %>% 
+  mutate_if(is.numeric, as.factor)
+m2 = model.matrix(~., dat4[corIncl], contrasts.arg = lapply(dat4[,corIncl], contrasts, contrasts=FALSE)) # making model matrix without a baseline category for the variables
+corM2 = cor(m2[,-1])
 
-y = nhdata[,7]
+# View(abs(corM2))
+View(abs(corM2[81:89,81:89]))
+ View(abs(corM2[63:89,84:89])) # ranking the variables 
+ 
+ 
+# model matrix for lasso
+names(dat4)
+dat5 = dat4 %>% 
+  select(-c(pest_use)) # after looking at correlation
+names(dat5)
+
+covInd = c(2:23)[-c(6)] # removing high bp 
+m = model.matrix(~ . -1, data = dat5[,covInd], contrasts.arg = lapply(dat5[,covInd], contrasts, contrasts=FALSE))
+y = as.numeric(dat5[,7]) - 1 # high bp
+
+# fixing foldids
+nfolds = 15
+foldid = 1 + (1:nrow(m) %% nfolds) # no need to specify seed if setting fold ID
+ysel = cbind(y, dat5$incl_dr2, dat5$incl_fas, dat5$incl_voc, dat5$incl_env)
+coef_mat = NULL
+for(i in 1:ncol(ysel)){
+  (lmb = cv.glmnet(m,ysel[,i],alpha = 1, family = "binomial", intercept=T, type.measure ="auc",foldid=foldid)$lambda.1se)
+  coef_vec = glmnet(x=m, y=ysel[,i], family="binomial", intercept=T, type.measure = "auc", lambda = lmb) %>% coef(.)
+  coef_mat = cbind(coef_mat, coef_vec)
+}
+colnames(coef_mat) = c('high_bp', 'dietary', 'fas', 'voc', 'env')
+coef_mat
 
 
-# cross validation to select lambda
-set.seed(1237489)
-mod_cv = cv.glmnet(x=m, y=y, family="binomial", intercept=F, nfolds=5)
-coef(mod_cv, c(mod_cv$lambda.1se))
+# removing ignorable variables --------------------------------------------
+dat6 = dat5 %>% 
+  select(-c(dep_severity,smk2_exp,potassium_lvl, den_vst, incl_env, incl_voc))
 
-mod_incl_cv_voc = cv.glmnet(x=m, y=nhdata$incl_voc, family="binomial", intercept=F, nfolds = 5)
-coef(mod_incl_cv_voc, c(mod_incl_cv_voc$lambda.1se))
+names(dat6)
 
-mod_incl_cv_dr1 = cv.glmnet(x=m, y=nhdata$incl_dr1, family="binomial", intercept=F, nfolds = 5)
-coef(mod_incl_cv_dr1, c(mod_incl_cv_dr1$lambda.1se))
+covInd = c(2:19)[-c(6)] # removing high bp 
+m = model.matrix(~ . -1, data = dat6[,covInd], contrasts.arg = lapply(dat6[,covInd], contrasts, contrasts=FALSE))
+y = as.numeric(dat6[,7]) - 1 # high bp
 
-mod_incl_cv_urn = cv.glmnet(x=m, y=nhdata$incl_urn, family="binomial", intercept=F, nfolds = 5)
-coef(mod_incl_cv_urn, c(mod_incl_cv_urn$lambda.1se))
+# fixing foldids
+nfolds = 15
+foldid = 1 + (1:nrow(m) %% nfolds) # no need to specify seed if setting fold ID
+ysel = cbind(y, dat6$incl_dr2, dat6$incl_fas)
+coef_mat = NULL
+for(i in 1:ncol(ysel)){
+  (lmb = cv.glmnet(m,ysel[,i],alpha = 1, family = "binomial", intercept=T, type.measure ="auc",foldid=foldid)$lambda.1se)
+  coef_vec = glmnet(x=m, y=ysel[,i], family="binomial", intercept=T, type.measure = "auc", lambda = lmb) %>% coef(.)
+  coef_mat = cbind(coef_mat, coef_vec)
+}
+colnames(coef_mat) = c('high_bp', 'dietary', 'fas')
+coef_mat
 
-mod_incl_cv_fas = cv.glmnet(x=m, y=nhdata$incl_fas, family="binomial", intercept=F, nfolds = 5)
-coef(mod_incl_cv_fas, c(mod_incl_cv_fas$lambda.1se))
-
-
-
-
-# 8192 8193 12648 17054
-
-
-
-
-
+saveRDS(dat6, file=here("nhanes/data/nhanes_final.rds"))
 

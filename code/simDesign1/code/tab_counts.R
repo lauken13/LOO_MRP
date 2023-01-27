9b69a43a41bd6c1a5b20f51a5a8b3dd61b2f2e9e
swenk238 <swen.kuh@monash.edu>
2023-01-27 12:25:13 +1100

    folder updates

--- code/simDesign1/code/tab_counts.R
+++ code/simDesign1/code/tab_counts.R
@@ -3,24 +3,22 @@ tabX1 = lapply(popn_data_list, function(x){ table(x$X1)}) %>%
   apply(., 2, mean) %>% 
   round(.)
 
-
 tabX2 = lapply(popn_data_list, function(x){ table(x$X2)}) %>% 
   do.call(rbind, .) %>% 
   apply(., 2, mean) %>% 
   round(.)
-
 
 tabX3 = lapply(popn_data_list, function(x){ table(x$X3)}) %>% 
   do.call(rbind, .) %>% 
   apply(., 2, mean) %>% 
   round(.)
 
-
 tabX4 = lapply(popn_data_list, function(x){ table(x$X4)}) %>% 
   do.call(rbind, .) %>% 
   apply(., 2, mean) %>% 
   round(.)
 
+# population counts
 popn_counts = rbind(tabX1, tabX2, tabX3, tabX4) %>% 
   as_tibble() %>% 
  # rownames_to_column(var = 'sae') %>% 
@@ -32,12 +30,10 @@ sampX1 = lapply(samp_data_list, function(x){ table(x$X1)}) %>%
   apply(., 2, mean) %>% 
   round(.)
 
-
 sampX2 = lapply(samp_data_list, function(x){ table(x$X2)}) %>% 
   do.call(rbind, .) %>% 
   apply(., 2, mean) %>% 
   round(.)
-
 
 sampX3 = lapply(samp_data_list, function(x){ table(x$X3)}) %>% 
   do.call(rbind, .) %>% 
@@ -49,11 +45,9 @@ sampX4 = lapply(samp_data_list, function(x){ table(x$X4)}) %>%
   apply(., 2, mean) %>% 
   round(.)
 
+# sample counts
 samp_counts = rbind(sampX1, sampX2, sampX3, sampX4) %>% 
   as_tibble() %>% 
   # rownames_to_column(var = 'sae') %>% 
   mutate(sae =  paste0('X',1:4)) %>% 
   pivot_longer(`1`:`5`, names_to = 'group') 
-
-
-
